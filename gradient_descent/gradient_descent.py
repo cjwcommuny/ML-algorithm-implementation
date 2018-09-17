82c90eea400265c299ba56ee62fd42498fe5dfc9
cjwcommuny <cjwcommuny@outlook.com>
2018-09-17 08:17:36 +0800

    complete SGD

--- gradient_descent/gradient_descent.py
+++ gradient_descent/gradient_descent.py
@@ -19,19 +19,39 @@ def batchGradientDescent(computeGradient, computeFunc, epsilon, step, x0: "initi
     return x, f_current, k
 
 
+def linearTransformation(X, theta):
+    if X.shape[1] == 1 and theta.shape[1] == 1:
+        return np.dot(theta.T, X)
+    else:
+        return np.dot(X, theta)
+
+
+def linearMatrix(X, theta):
+    return theta
+
+
 def computeCostFunc(X: "dataset", y: "label", theta: "parameter", h: "function, h(X, theta)"):
     return 0.5 * np.sum((h(X, theta) - y.T)** 2)
-    
+
 def computeGradient(X: "dataset", y: "label", theta: "parameter", h_gradient: "gradient of function, h_gradient(X, theta)", h: "function, h(X, theta)"):
     return (h(X, theta) - y.T) * h_gradient(X, theta)
 
 
-def stochasticGradientDescent(X: "dataset", h: "compute h(X)", epsilon):
+def stochasticGradientDescent(X: "dataset", y: "label", theta0, epsilon, k_max,
+                              step, h=computeCostFunc):
     m = len(X)  #size of dataset
     k = 0
-    
+    theta = theta0
+    f_previous = 0
+    f_current = computeCostFunc(X, y, theta, linearTransformation)
+
     while True:
         if k > k_max:
             break
         for i in range(m):
-            theta[i] = 
+            theta.T += -step * computeGradient(X, y, theta, linearMatrix, linearTransformation)
+        f_previous = f_current
+        f_current = computeCostFunc(X, y, theta, linearMatrix)
+        if abs(f_current - f_previous) < epsilon:
+            break
+    return theta, f_current, k
--- gradient_descent/test.py
+++ gradient_descent/test.py
@@ -1,6 +1,7 @@
 import unittest
 import numpy as np
 import gradient_descent
+import random
 
 def test_squareFunc():
     squareFunc = lambda x: x ** 2
@@ -13,4 +14,21 @@ def test_squareFunc():
     print(minimalPoint, f_current, k)
 
 
-test_squareFunc()
+def test_SGD():
+    theta0 = np.array([100, 50])
+    dimension = 2
+    m = 100
+    X = np.zeros([m, dimension])
+    y = np.zeros([m, 1])
+    theta_original = np.array([3, 5])
+    epsilon = 0.01
+    k_max = 100
+    step = 1
+    for i in range(m):
+        X[i][0], X[i][1] = random.uniform(-100, 100), random.uniform(-100, 100)
+        y[i] = gradient_descent.linearTransformation(X[i].T, theta_original)
+    theta, f, k = gradient_descent.stochasticGradientDescent(X, y, theta0, epsilon, k_max, step)
+    print(theta, f, k)
+
+#test_squareFunc()
+test_SGD()

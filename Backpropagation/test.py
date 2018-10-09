e408018680af468635485e6720bf73841cb7fe50
cjwcommuny <cjwcommuny@outlook.com>
2018-10-09 21:55:12 +0800

    modify test.py in backpropagation

--- Backpropagation/test.py
+++ Backpropagation/test.py
@@ -1,21 +1,19 @@
-import numpy as np
-from Backpropagation import *
 import sys
-sys.path.append("../")
-import Infrastracture
+import numpy as np
+from Backpropagation import Data, Parameter, Operation, forwardPropagation
 
 
 def test():
     X = np.mat([[0, 0], [0, 1], [1, 0], [1, 1]])
-    y = np.mat([[0],[1],[1],[0]])
-    W = [np.mat([[1,1],[1,1]]), np.mat([[1],[-2]])]
+    y = np.mat([[0], [1], [1], [0]])
+    W = [np.mat([[1, 1], [1, 1]]), np.mat([[1], [-2]])]
     B = [np.mat([[0], [-1]]), np.mat([0])]
     lam = 0
-    ReLU = lambda z: np.multiply(z > 0, z)
     data = Data(X, y)
     param = Parameter(W, B, lam)
-    op = Operation(ReLU)
+    op = Operation(lambda z: np.multiply(z > 0, z))
     error = forwardPropagation(data, param, op)
     print(error)
-    
+
+
 test()

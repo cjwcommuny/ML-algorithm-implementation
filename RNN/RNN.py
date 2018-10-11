5d999bfdf16d36a140950d426a3a11c78a968bcd
cjwcommuny <cjwcommuny@outlook.com>
2018-10-11 23:30:03 +0800

    init RNN

---
+++ RNN/RNN.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+
+class RNN:
+    def __init__(self, inputSize, hiddenLayerCount):
+        pass
+
+
+    def __call__(self, inputTensor):
+        pass
+
+
+    def forwardPropagation(self, inputTensor):
+        pass
+
